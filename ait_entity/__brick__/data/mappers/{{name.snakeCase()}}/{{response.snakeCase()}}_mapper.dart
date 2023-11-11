64198e94cce90f6b1d1d38c71f72023db84ba224
VikralAIT <96177519+VikralAIT@users.noreply.github.com>
2023-11-11 19:18:29 +0700

    feat (ait_entity) : create mapper

--- ait_entity/__brick__/data/mappers/{{name.snakeCase()}}/{{response.snakeCase()}}_mapper.dart
+++ ait_entity/__brick__/data/mappers/{{name.snakeCase()}}/{{response.snakeCase()}}_mapper.dart
@@ -1,4 +1,60 @@
+import '../../models/{{name.snakeCase()}}/{{response.snakeCase()}}.dart';
+import '../../../domain/entities/{{name.snakeCase()}}/{{name.snakeCase()}}.dart';
 
 extension {{response}}Mapper on {{response}} {
- 
+  {{entity}} get toEntity {
+      return {{entity}}(
+    {{#object}}
+    {{^is_object}}
+      {{^is_list}}{{name.camelCase()}}: {{name.camelCase()}} ?? '',{{/is_list}}
+      {{#is_list}}{{name.camelCase()}}: {{name.camelCase()}} ?? [],{{/is_list}}
+    {{/is_object}}
+    {{#is_object}}
+      {{^is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.toEntity ?? const {{type.pascalCase()}}Response().toEntity,{{/is_list}}
+      {{#is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.map((e)=>e.toEntity).toList() ?? [],{{/is_list}}
+    {{/is_object}}
+    {{/object}}
+      );
+    }
   }
+
+{{#object}}
+{{#is_object}}
+extension {{type.pascalCase()}}ResponseMapper on {{type.pascalCase()}}Response {
+  {{type.pascalCase()}} get toEntity {
+      return {{type.pascalCase()}}(
+    {{#object}}
+    {{^is_object}}
+      {{^is_list}}{{name.camelCase()}}: {{name.camelCase()}} ?? '',{{/is_list}}
+      {{#is_list}}{{name.camelCase()}}: {{name.camelCase()}} ?? [],{{/is_list}}
+    {{/is_object}}
+    {{#is_object}}
+      {{^is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.toEntity ?? const {{type.pascalCase()}}Response().toEntity,{{/is_list}}
+      {{#is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.map((e)=>e.toEntity).toList() ?? [],{{/is_list}}
+    {{/is_object}}
+    {{/object}}
+      );
+    }
+  }
+{{#object}}
+{{#is_object}}
+extension {{type.pascalCase()}}ResponseMapper on {{type.pascalCase()}}Response {
+  {{type.pascalCase()}} get toEntity {
+      return {{type.pascalCase()}}(
+    {{#object}}
+    {{^is_object}}
+      {{^is_list}}{{name.camelCase()}}: {{name.camelCase()}} ?? '',{{/is_list}}
+      {{#is_list}}{{name.camelCase()}}: {{name.camelCase()}} ?? [],{{/is_list}}
+    {{/is_object}}
+    {{#is_object}}
+      {{^is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.toEntity ?? const {{type.pascalCase()}}Response().toEntity,{{/is_list}}
+      {{#is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.map((e)=>e.toEntity).toList() ?? [],{{/is_list}}
+    {{/is_object}}
+    {{/object}}
+      );
+    }
+  }
+{{/is_object}}
+{{/object}}
+{{/is_object}}
+{{/object}}
--- ait_entity/__brick__/data/models/{{name.snakeCase()}}/{{response.snakeCase()}}.dart
+++ ait_entity/__brick__/data/models/{{name.snakeCase()}}/{{response.snakeCase()}}.dart
@@ -7,8 +7,14 @@ part '{{response.snakeCase()}}.g.dart';
 class {{response}} with _${{response}} {
   const factory {{response}}({
     {{#object}}
-    {{#is_list}} List<{{type.pascalCase()}}>? {{name.camelCase()}},{{/is_list}}
-    {{^is_list}} {{type.pascalCase()}}? {{name.camelCase()}},{{/is_list}}
+    {{#is_object}}
+      {{#is_list}} List<{{type.pascalCase()}}Response>? {{name.camelCase()}},{{/is_list}}
+      {{^is_list}} {{type.pascalCase()}}Response? {{name.camelCase()}},{{/is_list}}
+    {{/is_object}}
+    {{^is_object}}
+      {{#is_list}} List<{{type.pascalCase()}}>? {{name.camelCase()}},{{/is_list}}
+      {{^is_list}} {{type.pascalCase()}}? {{name.camelCase()}},{{/is_list}}
+    {{/is_object}}
     {{/object}}
   }) = _{{response}};
 
@@ -18,28 +24,40 @@ class {{response}} with _${{response}} {
 {{#object}}
 {{#is_object}}
 @freezed
-class {{type.pascalCase()}} with _${{type.pascalCase()}} {
-  const factory {{type.pascalCase()}}({
+class {{type.pascalCase()}}Response with _${{type.pascalCase()}}Response {
+  const factory {{type.pascalCase()}}Response({
     {{#object}}
-    {{#is_list}} List<{{type.pascalCase()}}>? {{name.camelCase()}},{{/is_list}}
-    {{^is_list}} {{type.pascalCase()}}? {{name.camelCase()}},{{/is_list}}
+    {{#is_object}}
+      {{#is_list}} List<{{type.pascalCase()}}Response>? {{name.camelCase()}},{{/is_list}}
+      {{^is_list}} {{type.pascalCase()}}Response? {{name.camelCase()}},{{/is_list}}
+    {{/is_object}}
+    {{^is_object}}
+      {{#is_list}} List<{{type.pascalCase()}}>? {{name.camelCase()}},{{/is_list}}
+      {{^is_list}} {{type.pascalCase()}}? {{name.camelCase()}},{{/is_list}}
+    {{/is_object}}
     {{/object}}
-  }) = _{{type.pascalCase()}};
-  factory {{type.pascalCase()}}.fromJson(Map<String, dynamic> json) =>
-      _${{type.pascalCase()}}FromJson(json);
+  }) = _{{type.pascalCase()}}Response;
+  factory {{type.pascalCase()}}Response.fromJson(Map<String, dynamic> json) =>
+      _${{type.pascalCase()}}ResponseFromJson(json);
 }
   {{#object}}
   {{#is_object}}
   @freezed
-  class {{type.pascalCase()}} with _${{type.pascalCase()}} {
-    const factory {{type.pascalCase()}}({
+  class {{type.pascalCase()}}Response with _${{type.pascalCase()}}Response {
+    const factory {{type.pascalCase()}}Response({
       {{#object}}
-      {{#is_list}} List<{{type.pascalCase()}}>? {{name.camelCase()}},{{/is_list}}
-      {{^is_list}} {{type.pascalCase()}}? {{name.camelCase()}},{{/is_list}}
+      {{#is_object}}
+        {{#is_list}} List<{{type.pascalCase()}}Response>? {{name.camelCase()}},{{/is_list}}
+        {{^is_list}} {{type.pascalCase()}}Response? {{name.camelCase()}},{{/is_list}}
+      {{/is_object}}
+      {{^is_object}}
+        {{#is_list}} List<{{type.pascalCase()}}>? {{name.camelCase()}},{{/is_list}}
+        {{^is_list}} {{type.pascalCase()}}? {{name.camelCase()}},{{/is_list}}
+      {{/is_object}}
       {{/object}}
-    }) = _{{type.pascalCase()}};
-    factory {{type.pascalCase()}}.fromJson(Map<String, dynamic> json) =>
-        _${{type.pascalCase()}}FromJson(json);
+    }) = _{{type.pascalCase()}}Response;
+    factory {{type.pascalCase()}}Response.fromJson(Map<String, dynamic> json) =>
+        _${{type.pascalCase()}}ResponseFromJson(json);
   }
   {{/is_object}}
   {{/object}}

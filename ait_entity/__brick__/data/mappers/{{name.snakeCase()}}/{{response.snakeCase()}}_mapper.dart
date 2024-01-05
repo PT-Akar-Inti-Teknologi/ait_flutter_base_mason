396e53459fde4262c7e325e728e670cc0784b0ad
VikralAIT <96177519+VikralAIT@users.noreply.github.com>
2024-01-05 12:10:11 +0700

    feat (ait_entity) : ait entity now support request models

--- ait_entity/__brick__/data/mappers/{{name.snakeCase()}}/{{response.snakeCase()}}_mapper.dart
+++ ait_entity/__brick__/data/mappers/{{name.snakeCase()}}/{{response.snakeCase()}}_mapper.dart
@@ -1,60 +1,9 @@
 import '../../models/{{name.snakeCase()}}/{{response.snakeCase()}}.dart';
 import '../../../domain/entities/{{name.snakeCase()}}/{{name.snakeCase()}}.dart';
 
-extension {{response}}Mapper on {{response}} {
-  {{entity}} get toEntity {
-      return {{entity}}(
-    {{#object}}
-    {{^is_object}}
-      {{^is_string}}{{name.camelCase()}}: {{name.camelCase()}} ?? {{default}},{{/is_string}}
-      {{#is_string}}{{name.camelCase()}}: {{name.camelCase()}} ?? '{{default}}',{{/is_string}}
-    {{/is_object}}
-    {{#is_object}}
-      {{^is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.toEntity ?? const {{type}}Response().toEntity,{{/is_list}}
-      {{#is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.map((e)=>e.toEntity).toList() ?? [],{{/is_list}}
-    {{/is_object}}
-    {{/object}}
-      );
-    }
-  }
-
+{{> mapper_base }}
 {{#object}}
 {{#is_object}}
-extension {{type}}ResponseMapper on {{type}}Response {
-  {{type}} get toEntity {
-      return {{type}}(
-    {{#object}}
-    {{^is_object}}
-      {{^is_string}}{{name.camelCase()}}: {{name.camelCase()}} ?? {{default}},{{/is_string}}
-      {{#is_string}}{{name.camelCase()}}: {{name.camelCase()}} ?? '{{default}}' ,{{/is_string}}
-    {{/is_object}}
-    {{#is_object}}
-      {{^is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.toEntity ?? const {{type}}Response().toEntity,{{/is_list}}
-      {{#is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.map((e)=>e.toEntity).toList() ?? [],{{/is_list}}
-    {{/is_object}}
-    {{/object}}
-      );
-    }
-  }
-{{#object}}
-{{#is_object}}
-extension {{type}}ResponseMapper on {{type}}Response {
-  {{type}} get toEntity {
-      return {{type}}(
-    {{#object}}
-    {{^is_object}}
-      {{^is_string}}{{name.camelCase()}}: {{name.camelCase()}} ?? {{default}},{{/is_string}}
-      {{#is_string}}{{name.camelCase()}}: {{name.camelCase()}} ?? '{{default}}' ,{{/is_string}}
-    {{/is_object}}
-    {{#is_object}}
-      {{^is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.toEntity ?? const {{type}}Response().toEntity,{{/is_list}}
-      {{#is_list}}{{name.camelCase()}}: {{name.camelCase()}}?.map((e)=>e.toEntity).toList() ?? [],{{/is_list}}
-    {{/is_object}}
-    {{/object}}
-      );
-    }
-  }
-{{/is_object}}
-{{/object}}
+{{> mapper_child_base}}
 {{/is_object}}
 {{/object}}

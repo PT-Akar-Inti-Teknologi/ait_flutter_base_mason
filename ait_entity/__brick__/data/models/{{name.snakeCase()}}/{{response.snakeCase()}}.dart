00de6947fae1ba3942f49edd14ac358d0ae97414
VikralAIT <96177519+VikralAIT@users.noreply.github.com>
2024-01-05 11:18:34 +0700

    feat (ait_entity) : make response generator generate accordingly to param

--- ait_entity/__brick__/data/models/{{name.snakeCase()}}/{{response.snakeCase()}}.dart
+++ ait_entity/__brick__/data/models/{{name.snakeCase()}}/{{response.snakeCase()}}.dart
@@ -3,63 +3,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part '{{response.snakeCase()}}.freezed.dart';
 part '{{response.snakeCase()}}.g.dart';
 
-@freezed
-class {{response}} with _${{response}} {
-  const factory {{response}}({
-    {{#object}}
-    {{#is_object}}
-      {{#is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} List<{{type}}Response>? {{name.camelCase()}},{{/is_list}}
-      {{^is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} {{type}}Response? {{name.camelCase()}},{{/is_list}}
-    {{/is_object}}
-    {{^is_object}}
-      {{#is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} List<{{type}}>? {{name.camelCase()}},{{/is_list}}
-      {{^is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} {{type}}? {{name.camelCase()}},{{/is_list}}
-    {{/is_object}}
-    {{/object}}
-  }) = _{{response}};
-
-  factory {{response}}.fromJson(Map<String, dynamic> json) =>
-      _${{response}}FromJson(json);
-}
+{{> response_base }}
 {{#object}}
 {{#is_object}}
-@freezed
-class {{type}}Response with _${{type}}Response {
-  const factory {{type}}Response({
-    {{#object}}
-    {{#is_object}}
-      {{#is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} List<{{type}}Response>? {{name.camelCase()}},{{/is_list}}
-      {{^is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} {{type}}Response? {{name.camelCase()}},{{/is_list}}
-    {{/is_object}}
-    {{^is_object}}
-      {{#is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} List<{{type}}>? {{name.camelCase()}},{{/is_list}}
-      {{^is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} {{type}}? {{name.camelCase()}},{{/is_list}}
-    {{/is_object}}
-    {{/object}}
-  }) = _{{type}}Response;
-  factory {{type}}Response.fromJson(Map<String, dynamic> json) =>
-      _${{type}}ResponseFromJson(json);
-}
-  {{#object}}
-  {{#is_object}}
-  @freezed
-  class {{type}}Response with _${{type}}Response {
-    const factory {{type}}Response({
-      {{#object}}
-      {{#is_object}}
-        {{#is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} List<{{type}}Response>? {{name.camelCase()}},{{/is_list}}
-        {{^is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} {{type}}Response? {{name.camelCase()}},{{/is_list}}
-      {{/is_object}}
-      {{^is_object}}
-        {{#is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} List<{{type}}>? {{name.camelCase()}},{{/is_list}}
-        {{^is_list}}{{#is_snake}}@JsonKey(name:'{{name.snakeCase()}}'){{/is_snake}} {{type}}? {{name.camelCase()}},{{/is_list}}
-      {{/is_object}}
-      {{/object}}
-    }) = _{{type}}Response;
-    factory {{type}}Response.fromJson(Map<String, dynamic> json) =>
-        _${{type}}ResponseFromJson(json);
-  }
-  {{/is_object}}
-  {{/object}}
+{{> response_child_base}}
 {{/is_object}}
 {{/object}}
--- ait_entity/hooks/pre_gen.dart
+++ ait_entity/hooks/pre_gen.dart
@@ -7,9 +7,10 @@ part 'pre_gen_utils/remap.dart';
 
 void run(HookContext context) async {
   final name = context.vars['name'];
+  final isParam = context.vars['is_param'];
   final entity = getEntityName(context, context.vars);
-  final response = '${name.toString().pascalCase}Response';
-  context.vars.removeWhere((key, value) => key == 'name');
+  final response = getResponseName(context, context.vars);
+  context.vars.removeWhere((key, value) => key == 'name' || key == 'is_param');
   final data = await context.vars;
   context.vars = {};
   final remapedData = remap(data);
@@ -17,6 +18,7 @@ void run(HookContext context) async {
     "name": name,
     "entity": entity,
     "response": response,
+    "is_param": isParam,
     "object": remapedData,
   };
   context.vars.addAll(joinedData);
@@ -27,4 +29,11 @@ String getEntityName(HookContext context, Map<String, dynamic> vars) {
   final name = context.vars['name'];
   final entityName = '${name.toString().pascalCase}';
   return isParam == true ? '$entityName' + 'Param' : entityName;
+}
+
+String getResponseName(HookContext context, Map<String, dynamic> vars) {
+  final bool? isParam = context.vars['is_param'];
+  final name = context.vars['name'];
+  final responseName = '${name.toString().pascalCase}';
+  return isParam == true ? responseName + 'Request' : responseName + 'Response';
 }

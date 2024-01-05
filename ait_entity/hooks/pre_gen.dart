8b23953750c4302764d2003003889e43519e672e
VikralAIT <96177519+VikralAIT@users.noreply.github.com>
2024-01-05 09:52:54 +0700

    feat (ait_entity) : add ability to give entity name as param

--- ait_entity/hooks/pre_gen.dart
+++ ait_entity/hooks/pre_gen.dart
@@ -7,7 +7,7 @@ part 'pre_gen_utils/remap.dart';
 
 void run(HookContext context) async {
   final name = context.vars['name'];
-  final entity = '${name.toString().pascalCase}';
+  final entity = getEntityName(context, context.vars);
   final response = '${name.toString().pascalCase}Response';
   context.vars.removeWhere((key, value) => key == 'name');
   final data = await context.vars;
@@ -20,4 +20,11 @@ void run(HookContext context) async {
     "object": remapedData,
   };
   context.vars.addAll(joinedData);
+}
+
+String getEntityName(HookContext context, Map<String, dynamic> vars) {
+  final bool? isParam = context.vars['is_param'];
+  final name = context.vars['name'];
+  final entityName = '${name.toString().pascalCase}';
+  return isParam == true ? '$entityName' + 'Param' : entityName;
 }

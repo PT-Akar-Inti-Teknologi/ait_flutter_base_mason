a337efc6a86ddaee173072488f44de1dd8755014
VikralAIT <96177519+VikralAIT@users.noreply.github.com>
2024-01-11 16:01:14 +0700

    fix (ait_entity) : eror on generate response due to request

--- ait_entity/__brick__/data/mappers/{{name.snakeCase()}}/{{response.snakeCase()}}_mapper.dart
+++ ait_entity/__brick__/data/mappers/{{name.snakeCase()}}/{{response.snakeCase()}}_mapper.dart
@@ -1,9 +1,20 @@
 import '../../models/{{name.snakeCase()}}/{{response.snakeCase()}}.dart';
 import '../../../domain/entities/{{name.snakeCase()}}/{{name.snakeCase()}}{{#is_param}}_param{{/is_param}}.dart';
 
+{{#is_param}}
+{{> mapper_param_base }}
+{{#object}}
+{{#is_object}}
+{{> mapper_param_child_base}}
+{{/is_object}}
+{{/object}}
+{{/is_param}}
+
+{{^is_param}}
 {{> mapper_base }}
 {{#object}}
 {{#is_object}}
 {{> mapper_child_base}}
 {{/is_object}}
 {{/object}}
+{{/is_param}}
